af4feb59cc4154dc70aa85b1071e3cd2c6e3ce94
kibs2021 <kibstechnologies2021@gmail.com>
2021-11-21 15:56:38 +0300

    python exerises

--- Fibonnaci_serious.py
+++ Fibonnaci_serious.py
@@ -8,18 +8,20 @@ like this: 1, 1, 2, 3, 5, 8, 13, …)
 
 '''
 
-#def gen_fibonnaci(num="Number of fibonacci sequences: "):
-#    x=[]
-
-
-num=int(input("How many fibonnaci numbers do you want to generate? "))
-
-if num==0:
-    fib=[]
-
-if num==1:
-    fib=[1]
-if num==2:
-    fib=[1,1]
-if num>2:
-    
+def fibonacci():
+    num = int(input("How many fibonnaci numbers do you want to generate?:  "))
+    i = 1
+    if num == 0:
+        fib = []
+    elif num == 1:
+        fib = [1]
+    elif num == 2:
+        fib = [1,1]
+    elif num > 2:
+        fib = [1,1]
+        while i < (num - 1):
+            fib.append(fib[i] + fib[i-1])
+            i += 1
+    return fib
+print(fibonacci())
+input()
